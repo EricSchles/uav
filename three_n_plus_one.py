c2ebab1b5fe9a41a7400b4d1e1bb168034ed8bff
Eric Schles <ericschles@gmail.com>
2016-04-20 09:55:22 -0400

    updating

--- three_n_plus_one.py
+++ three_n_plus_one.py
@@ -1,32 +1,36 @@
 import sys
 
-lookup = {}
+def memoized(fn):
+    memoized_lengths = [0] * 1000000
+    def fn2(n):
+        if not memoized_lengths[n]:
+            memoized_lengths[n] = fn(n)
+        return memoized_lengths[n]
+    return fn2
+
+@memoized
+def cycle_length(n):
+    if n == 1: return 1
+    if n % 2:
+        return cycle_length(n * 3 + 1) + 1
+    return cycle_length(n / 2) + 1
+    
+def problem(i,j):
+    return max([cycle_length(x) for x in range(i, j+1)])
+
 user_input = sys.stdin.read().split("\n")
 switched = False
 for line in user_input:
     if line == '':
         print()
-        #continue
-        sys.exit(0)
+        continue
+        #sys.exit(0)
     start,end = int(line.split(" ")[0]),int(line.split(" ")[1])
     if start > end:
         switched = True
-        start,end = end,start 
-    cycles = []
-    for n in range(start,end+1):
-        count = 1
-        if n in lookup:
-            count = lookup[n]
-        else:
-            while n != 1:
-                if n%2!=0:
-                    n = 3*n+1
-                else:
-                    n /= 2
-                count += 1
-            lookup[n] = count
-        cycles.append(count)
+        start,end = end,start
+    sol = problem(start,end)
     if switched:
         start,end = end,start
         switched = False
-    print("{} {} {}".format(start,end,max(cycles)))
+    print("{} {} {}".format(start,end,sol))
