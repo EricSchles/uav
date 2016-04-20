2cf292d48e3b4eedc9ff978b0585d1fe18d71d23
Eric Schles <ericschles@gmail.com>
2016-04-20 09:20:12 -0400

    updating

--- three_n_plus_one.py
+++ three_n_plus_one.py
@@ -15,49 +15,26 @@ def process_input(string):
     start,end = [int(elem) for elem in string.split(" ") if elem != '']
     return start,end
 
-def dyn_binary_search(elem,array):
-    firsts = [0]
-    lasts = [len(array)-1]
-    while firsts[-1] <= lasts[-1]:
-        midpoint = (firsts[-1] + lasts[-1]) // 2
-        if elem == array[midpoint]:
-            return True
-        elif elem < array[midpoint]:
-            lasts.append(midpoint-1)
-        else:
-            firsts.append(midpoint+1)
-    return False
+start_time = time.time()
 
-def main():
-    lookup = {}
-    already_seen = []
-    user_input = sys.stdin.read().split("\n")
-    switched = False
-    for line in user_input:
-        if line == '':
-            print()
-            continue
-            #sys.exit(0)
-        start,end = process_input(line)
-        if start > end:
-            switched = True
-            start,end = end,start 
-        cycles = []
-        for i in range(start,end+1):
-            if dyn_binary_search(i,already_seen):
-                cycles.append(lookup[i])
-            else:
-                already_seen.append(i)
-                already_seen.sort()
-                lookup[i] = cycle_length(i)
-                cycles.append(lookup[i])
-            if switched:
-                start,end = end,start
-                switched = False
-        print(str(start)+" "+str(end)+" "+str(max(cycles)))
+user_input = sys.stdin.read().split("\n")
+switched = False
+for line in user_input:
+    if line == '':
+        print()
+        continue
+        #sys.exit(0)
+    start,end = [int(elem) for elem in line.split(" ") if elem != '']
+    if start > end:
+        switched = True
+        start,end = end,start 
+    cycles = []
+    for i in range(start,end+1):
+        cycles.append(cycle_length(i))
+    if switched:
+        start,end = end,start
+        switched = False
+    print(str(start)+" "+str(end)+" "+str(max(cycles)))
 
-if __name__ == '__main__':
-    start = time.time()
-    main()
-    time_diff = time.time() - start
-    print("Took"+str(time_diff))
+time_diff = time.time() - start_time
+print("Took"+str(time_diff))
