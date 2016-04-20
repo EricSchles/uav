5def0c6f8039e254bdb36c7ad8e2b4d84e8f4f25
Eric Schles <ericschles@gmail.com>
2016-04-20 12:22:27 -0400

    updating

--- three_n_plus_one.py
+++ three_n_plus_one.py
@@ -32,5 +32,12 @@ for line in user_input:
     a,b = int(line.split(" ")[0]),int(line.split(" ")[1])
     if a<b: start,end = a,b 
     else: start,end = b,a
-    
-    print("{} {} {}".format(start,end,sol))
+    cyclemax = -1
+    for n in range(start,end+1):
+        cycle=1
+        while n!=1:
+            if n %2 != 0: n = 3*n+1
+            else: n >>=1
+            cycle += 1
+        if cycle > cyclemax: cyclemax = cycle
+    print("{} {} {}".format(start,end,cyclemax))
